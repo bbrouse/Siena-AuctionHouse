023b2ff4c6b664370924a00ab0e23a9338e79b81
Ben Brouse <benjaminbrouse@gmail.com>
2012-02-16 03:17:01 -0500

    supplier events added

--- Supplier.java
+++ Supplier.java
@@ -3,13 +3,22 @@ import siena.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
 
-public class Supplier{
-
-	public int restock(String item, Integer num, int price, int balance)
-	{
+public class Supplier implements Notifiable{
+	
+	private String myID = "Supplier";
+	private ThinClient siena;
+	
+	public void restock(Notification e)
+	{ 
+		String item = e.getAttribute("item");
+		int num = e.getAttribute("number");
+		int price = e.getAttribute("price");
+		int balance = e.getAttribute("balance");
+		
 		if((num * price)>balance){
 			System.out.println("You do not have enough money to buy " + num + " " + item + "s.");
 		}
@@ -18,23 +27,78 @@ public class Supplier{
 			balance -= (num * price);
 			System.out.println("Successfully restocked " + item + ".");
 		}
-		return balance;
+
+		Notification out = new Notification();
+		out.putAttribute("SI_Event", "Sale_confirmation");
+		out.putAttribute("item", item);
+		out.putAttribute("number", item);
+		out.putAttribute("new_balance", balance);
+	    System.out.println("publishing supplier sale" + out.toString());
+	    try {
+			siena.publish(out);
+    	} catch (SienaException ex) {
+			System.err.println("Siena error:" + ex.toString());
+    	}
 	}
-
-	public static void main(String args[]) {
 	
-		try {
-			Supplier obj = new Supplier();
-			SI stub = (SI) UnicastRemoteObject.exportObject(obj, 0);
+	
+	public void notify(Notification e) {
+        System.out.println(myID + " just got this event:");
+        System.out.println(e.toString() + "\n");
+        if(e.getAttribute("AH_Event").equals("Restock")){
+        	restock(e);
+        }
+    };
 
-			// Bind the remote object's stub in the registry
-			Registry registry = LocateRegistry.getRegistry(9988);
-			registry.bind("SI", stub);
-
-			System.err.println("Supplier ready to operate");
-		} catch (Exception e) {
-			System.err.println("Supplier exception: " + e.toString());
-			e.printStackTrace();
-		}
+    public void notify(Notification [] s) { 
+    	System.out.println(myID + " just got a bunch of events:");
+    	for (int i=0; i<s.length; i++){
+    		System.out.println(s[i].toString() + "\n");
+    		if(e.getAttribute("AH_Event").equals("Restock")){
+    			restock(e);
+    		}
+        }
     }
+
+	public static void main(String[] args) {
+		if(args.length != 1) {
+		    System.err.println("Usage: Supplier <server-address>");
+		    System.exit(1);
+		}
+		
+		ThinClient mySiena;
+		try {
+			// accepts one argument with a String in the form <protocol>:<ipaddress>:<port>
+			siena = new ThinClient(args[0]);
+
+		    Filter f = new Filter();
+		    f.addConstraint("AH_Event", Op.EQ, "Restock"); 
+		    f.addConstraint("AH_Event", Op.EQ, "SI_payment");
+		    InterestedParty party = new InterestedParty();
+		    
+		    System.out.println("Supplier Subscribing: " + f.toString());
+		    try {
+		    	siena.subscribe(f, party);
+		    	try {
+		    		Thread.sleep(86400000);	// sleeps for 24 hours
+		    	} catch (java.lang.InterruptedException ex) {
+		    		System.out.println("supplier interrupted"); 
+		    	}
+		    	
+		    	System.out.println("supplier unsubscribing");
+		    	siena.unsubscribe(f, party);
+		    	
+		    } catch (SienaException ex) {
+		    		System.err.println("Siena error in supplier:" + ex.toString());
+		    }
+		    	
+		    	System.out.println("Supplier shutting down.");
+		    	siena.shutdown();
+		    	System.exit(0);
+		    	
+		} catch (Exception ex) {
+				ex.printStackTrace();
+				System.exit(1);
+		} 
+	    };
 }
